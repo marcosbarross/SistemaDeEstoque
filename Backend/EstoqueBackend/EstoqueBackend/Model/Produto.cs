f8aae3a8393ebbfdeac290b4753e8af8541887b7
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2023-04-10 20:18:15 -0300

    Cria model de produto

---
+++ Backend/EstoqueBackend/EstoqueBackend/Model/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EstoqueBackend.Model
+{
+    public class Produto
+    {
+        [Key]
+        public int? Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        [Required]
+        public int? Preco { get; set; }
+        [Required]
+        public int? QuantidadeRestante { get; set; }
+    }
+}
